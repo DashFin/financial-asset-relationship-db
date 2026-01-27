d701d302858bd040cabfa7305424c538f0e14bb1
mmo80 <33737944+mohavro@users.noreply.github.com>
2026-01-27 07:16:30 +0000

    Add json import to test_pr_agent_config_validation.py

--- tests/integration/test_pr_agent_config_validation.py
+++ tests/integration/test_pr_agent_config_validation.py
@@ -3,10 +3,11 @@
 from dataclasses import asdict
 from typing import Any, Dict, Optional, Tuple
 
+import json
 import gradio as gr
 import plotly.graph_objects as go
-
 import logic
+
 from src.analysis.formulaic_analysis import FormulaicAnalyzer
 from src.analysis.formulaic_visualizer import FormulaicVisualizer
 from src.constants import AppConstants as BaseConstants
